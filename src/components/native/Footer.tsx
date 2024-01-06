77b4d2e31c1cbe8722bdaa7828dd999538e92b69
larry <larrykingsten1@gmail.com>
2024-01-06 17:13:07 +0200

    removed the wrong link for Larry codes

--- src/components/native/Footer.tsx
+++ src/components/native/Footer.tsx
@@ -91,10 +91,7 @@ export default function Footer() {
 
         <div className="w-full bg-neutral-300 p-4 text-center text-neutral-700 dark:bg-neutral-700 dark:text-neutral-200">
           © 2024 Wellington's Constructions
-          <a
-            className="text-neutral-800 ml-4 dark:text-neutral-400"
-            href="https://tw-elements.com/"
-          >
+          <a className="text-neutral-800 ml-4 dark:text-neutral-400">
             Website Designed by Larry Codes
           </a>
         </div>
